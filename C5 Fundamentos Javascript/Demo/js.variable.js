b0b5ac43b8296ddbe1ef409c3fb673a1ad1fd45c
Mmonicajal <akimya.tienda@hotmail.com>
2024-04-03 21:28:40 -0300

    funciones y variables

---
+++ C5 Fundamentos Javascript/Demo/js.variable.js
@@ -0,0 +1,28 @@
+var nombre1 = "monica"
+var nombre2 = `monica`
+var numero1 = 123456789
+var numerodecimal = 12.3456789
+var numeronegativo = -12.3456789
+var numero2 = "123456789"
+var numero3 = `123456789`
+
+var booleano = true
+var booleano = false
+var nulo = null
+var indefinido = undefined
+
+// console.log(nombre1)
+// console.log(numerodecimal)
+// console.log(nombre2,booleano,nulo,booleano)
+// console.log(noexiste)
+
+console.log("operacion"+"de"+"concatenacion"+"de"+ "strings")
+console.log("operacion"+1234)
+console.log(5<3)
+
+console.log(typeof nombre1)
+console.log(typeof numero1)
+console.log(typeof numerodecimal)
+console.log(typeof booleano)
+console.log(typeof nulo)
+console.log(typeof indefinido)
